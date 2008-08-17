7a0a8d3a48d656298dbd212309f0ddbeaf411619
Collin VanDyck <gluedtomyseat@gmail.com>
2008-08-17 10:17:32 -0400

    Configured app to run with nginx, took out passenger deployment configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
-set :application, "didmyteamwin"
-set :user, "collin"
+set :application, "didmyteamwintoday"
+set :user, "deploy"
 set :host, "#{user}@gluedtomyseat.com"
 
 set :scm, :git
 set :repository, "git@github.com:oculardisaster/did-my-team-win.git"
 
-set :deploy_to, "/home/#{user}/web/rails/#{application}"
+set :deploy_to, "/var/www/apps/#{application}"
 set :runner, user
 set :ssh_options, { :forward_agent => true }
 set :branch, "master"
@@ -14,22 +14,10 @@ role :app, "#{host}"
 role :web, "#{host}"
 role :db,  "#{host}", :primary => true
 
-namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-end
-
 after 'deploy:symlink' do
   run "cp #{deploy_to}/shared/database.yml #{deploy_to}/current/config"
 end
 
 after "deploy" do
-  run "#{current_path}/script/fetch_scores"
+  run "RAILS_ENV=production #{current_path}/script/fetch_scores"
 end
